d059b40b58161e53afa53ccae0f333b4ed83b84f
jtriguerom <joseantoniotriguero@gmail.com>
2015-07-09 23:42:59 +0200

    Cambio v2
    Cambio v2

--- random.js
+++ random.js
@@ -1,4 +1,6 @@
 // Math.random() devuelve número aleatorio entre 0 y 1.
+//introducimos un cambio sobre el texto para generar una nueva version
+
 var numero = Math.random();
 
 var str =' MAYOR que 0,5';
